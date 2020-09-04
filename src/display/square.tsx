ea5612c0685a3d1b2dd364d5ab2a0666cb0109d6
Olaf <olafdavis@googlemail.com>
2020-09-04 15:41:16 +0100

    display soldier and sniper icons

--- src/display/square.tsx
+++ src/display/square.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import {TerrainTypes} from '../game-state/BoardState';
 import './square.css';
-import {faHome} from '@fortawesome/free-solid-svg-icons';
-import {faBuilding} from '@fortawesome/free-solid-svg-icons';
-import {faStream} from '@fortawesome/free-solid-svg-icons';
+import {faBuilding, faHome, faStream, faWalking, faCrosshairs} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import GamePiece, {PieceType} from "../game-objects/GamePiece";
 
 export type SquareProps = {
     terrainType: TerrainTypes;
     xCoord: number;
     yCoord: number;
+    piece?: GamePiece;
 }
 
-function Square({terrainType, xCoord, yCoord}: SquareProps) {
+function Square({terrainType, xCoord, yCoord, piece}: SquareProps) {
     const terrainIcon =
         terrainType === TerrainTypes.Building ? faHome
         : terrainType === TerrainTypes.Tower ? faBuilding
         : terrainType === TerrainTypes.Rubble ? faStream
         : null;
+    const pieceProperties =
+        piece?.getPieceType() == PieceType.Soldier ? { icon: faWalking, class: 'soldier-icon' }
+            : piece?.getPieceType() == PieceType.Sniper ? { icon: faCrosshairs, class: 'sniper-icon' }
+            : null;
     return (
         <div className='grid-item'>
             {terrainIcon && <FontAwesomeIcon icon={terrainIcon} className='terrain-icon' />}
+            {pieceProperties && <FontAwesomeIcon icon={pieceProperties.icon} className={pieceProperties.class} />}
         </div>
     )
 }
